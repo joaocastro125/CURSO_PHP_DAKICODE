ea978699345f6f5daa62a2bb8c8b0c548ce0fced
joaocastro <joaocastro_125@hotmail.com>
2023-05-10 20:34:39 -0300

    apredendo php basico

---
+++ modulo1/aula_04/index.php
@@ -0,0 +1,20 @@
+<?php
+	/*String*/
+	$nome = 'João';
+	/*Número ou Inteiro*/
+	$idade = '35';
+	/*Boolean*/
+	$bool = true;
+	/*Doubles*/
+	$double = 10.09;
+
+	/*Agora vou printar na tela minha 2 variáveis*/
+
+	echo 'Meu nome é '.$nome;
+	echo 'Minha idade é '.$idade;
+	
+
+
+	
+
+?>
---
+++ modulo1/aula_07/index.php
@@ -0,0 +1,17 @@
+<?php
+	$frase = 'Meu nome é ';
+	$nome = 'João';
+	$idade = 35;
+	$nomedaclasse = 'box';
+
+	define('NOME','João');
+
+	
+
+	//echo $frase.$nome.$idade;
+
+	//echo 'Olá meu nome é '.$nome;
+	//echo "Meu nome é $nome";
+
+	echo "<div class=\"$nomedaclasse\">Meu conteúdo da div</div> ";
+?>
